cc57d4c5ece33126cb1d85e1ca7cb25d4d123590
Yapnounkie <edmundopierre@yahoo.com>
2021-02-03 18:29:02 -0800

    add rack-cors

--- backend/backend/db/seeds.rb
+++ backend/backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+cus1 = Customer.create(name:"Ziac", username:"ronaldo", email:"ziac@yahoo.com")
+ord1 = Order.create(product_name: 'Glasses', seller: "Mr Lee", price: 12.3, customer_id: cus1.id)
